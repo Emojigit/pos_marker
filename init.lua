4e7ad7ea8a49994589c7356c63503605ac1324bb
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-10-18 16:00:25 +0800

    Use subcommand mod to be the subcommand handler

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local mod_storage = minetest.get_mod_storage()
-local mark_priv = "interact"
-local gmark_edit = "server"
-local gmark_go = "interact"
-local tp_priv = "teleport"
+local mark_priv = {interact=true}
+local gmark_edit = {server=true}
+local gmark_go = {interact=true}
+local tp_priv = {teleport=true}
 pos_marker = {}
 -- minetest.registered_chatcommands
 -- ObjectRef:set_pos(pos)
@@ -29,129 +29,149 @@ pos_marker.get = function(user, name)
 	end
 end
 
-minetest.register_chatcommand("marker",{
-	description = "Config Markers",
-	params = "<get/set/override/tp> <marker name>",
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		if not(player) then
-			return false, "Not a player!"
-		end
-		local pos = player:getpos()
-		local subcommand, markName = param:match('^(%S+)%s(.+)$')
-		if param == "" or not(subcommand) or not(markName) then
-			return false, "Invalid usage, see /help marker"
-		end
-		if subcommand == "set" then
-			if minetest.check_player_privs(name, mark_priv) then
-				local markers = minetest.deserialize(mod_storage:get_string(tostring(name)))
-				if markers and markers[markName] then
-					return false, "Can't set marker: Use `override` subcommand to override."
-				end
-				pos_marker.set(name,markName,pos)
-				return true, "Setted!"
-			else
-				return false, "No priv to do this!"
-			end
-		elseif subcommand == "override" then
-			if minetest.check_player_privs(name, mark_priv) then
-				local markers = minetest.deserialize(mod_storage:get_string(tostring(name)))
-				if not(markers and markers[markName]) then
-					return false, "Can't override marker: Use `set` subcommand to add a marker."
-				end
-				pos_marker.set(name,markName,pos)
-				return true, "Overrided!"
-			else
-				return false, "No priv to do this!"
-			end
-		elseif subcommand == "get" then
-			if minetest.check_player_privs(name, mark_priv) then
-				local mpos = pos_marker.get(name,markName)
-				if mpos then
-					return true, "The marker "..markName.." is at "..minetest.pos_to_string(mpos)
-				end
-				return false, "No this marker!"
-			else
-				return false, "No priv to do this!"
-			end
-		elseif subcommand == "tp" then
-			if minetest.check_player_privs(name, tp_priv) then
-				local mpos = pos_marker.get(name,markName)
-				if mpos then
-					player:set_pos(mpos)
-					return true, "The marker "..markName.." is at "..minetest.pos_to_string(mpos)
-				end
-				return false, "No this marker!"
-			end
-		else
-			return false, "Invalid subcommand, see /help marker"
-		end
-	end,
-})
-minetest.register_chatcommand("marks",minetest.registered_chatcommands.marker)
 
-minetest.register_chatcommand("gmarker",{
-	description = "Config Global Markers",
-	params = "<get/set/tp> <marker name>",
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		if not(player) then
-			return false, "Not a player!"
-		end
-		local pos = player:getpos()
-		local subcommand, markName = param:match('^(%S+)%s(.+)$')
-		if param == "" or not(subcommand) or not(markName) then
-			return false, "Invalid usage, see /help marker"
-		end
-		if subcommand == "set" then
-			if minetest.check_player_privs(name, gmark_edit) then
-				local markers = minetest.deserialize(mod_storage:get_string(tostring("\\SERVER\\")))
-				if markers[markName] then
+subcommands.register_command_with_subcommand("gmarker",{
+	description = "Test of subcommands",
+	_sc_def = {
+		set = {
+			description = "Set a marker",
+			privs = gmark_edit,
+			params = "<marker name>",
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local pos = player:get_pos()
+				local markers = minetest.deserialize(mod_storage:get_string(tostring(name)))
+				if markers and markers[param] then
 					return false, "Can't set marker: Use `override` subcommand to override."
 				end
-				pos_marker.set("\\SERVER\\",markName,pos)
+				pos_marker.set(name,param,pos)
 				return true, "Setted!"
-			else
-				return false, "No priv to do this!"
 			end
-		elseif subcommand == "override" then
-			if minetest.check_player_privs(name, mark_priv) then
-				local markers = minetest.deserialize(mod_storage:get_string(tostring("\\SERVER\\")))
-				if not(markers[markName]) then
+		},
+		override = {
+			description = "Override a command",
+			privs = gmark_edit,
+			params = "<marker name>",
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local pos = player:get_pos()
+				local markers = minetest.deserialize(mod_storage:get_string(tostring(name)))
+				if not(markers and markers[param]) then
 					return false, "Can't override marker: Use `set` subcommand to add a marker."
 				end
-				pos_marker.set("\\SERVER\\",markName,pos)
+				pos_marker.set(name,param,pos)
 				return true, "Overrided!"
-			else
-				return false, "No priv to do this!"
 			end
-		elseif subcommand == "get" then
-			if minetest.check_player_privs(name, mark_priv) then
-				local mpos = pos_marker.get("\\SERVER\\",markName)
+		},
+		get = {
+			description = "Get a marker's pos",
+			privs = mark_priv,
+			params = "<marker name>",
+			func = function(name,param)
+				local mpos = pos_marker.get(name,param)
 				if mpos then
-					return true, "The marker "..markName.." is at "..minetest.pos_to_string(mpos)
+					return true, "The marker "..param.." is at "..minetest.pos_to_string(mpos)
 				end
 				return false, "No this marker!"
-			else
-				return false, "No priv to do this!"
 			end
-		elseif subcommand == "tp" then
-			if minetest.check_player_privs(name, gmark_go) then
-				local mpos = pos_marker.get("\\SERVER\\",markName)
+		},
+		tp = {
+			description = "Teleport to a marker",
+			params = "<marker name>",
+			privs = gmark_go,
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local mpos = pos_marker.get(name,param)
 				if mpos then
 					player:set_pos(mpos)
-					return true, "The marker "..markName.." is at "..minetest.pos_to_string(mpos)
+					return true, "The marker "..param.." is at "..minetest.pos_to_string(mpos)
 				end
 				return false, "No this marker!"
-			else
-				return false, "No priv to do this!"
 			end
-		else
-			return false, "Invalid subcommand, see /help marker"
-		end
-	end,
+		},
+	},
+})
+
+
+subcommands.register_command_with_subcommand("marker",{
+	description = "Test of subcommands",
+	_sc_def = {
+		set = {
+			description = "Set a marker",
+			privs = mark_priv,
+			params = "<marker name>",
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local pos = player:get_pos()
+				local markers = minetest.deserialize(mod_storage:get_string(tostring("\\SERVER\\")))
+				if markers and markers[param] then
+					return false, "Can't set marker: Use `override` subcommand to override."
+				end
+				pos_marker.set("\\SERVER\\",param,pos)
+				return true, "Setted!"
+			end
+		},
+		override = {
+			description = "Override a command",
+			privs = mark_priv,
+			params = "<marker name>",
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local pos = player:get_pos()
+				local markers = minetest.deserialize(mod_storage:get_string(tostring("\\SERVER\\")))
+				if not(markers and markers[param]) then
+					return false, "Can't override marker: Use `set` subcommand to add a marker."
+				end
+				pos_marker.set("\\SERVER\\",param,pos)
+				return true, "Overrided!"
+			end
+		},
+		get = {
+			description = "Get a marker's pos",
+			privs = mark_priv,
+			params = "<marker name>",
+			func = function(name,param)
+				local mpos = pos_marker.get("\\SERVER\\",param)
+				if mpos then
+					return true, "The marker "..param.." is at "..minetest.pos_to_string(mpos)
+				end
+				return false, "No this marker!"
+			end
+		},
+		tp = {
+			description = "Teleport to a marker",
+			params = "<marker name>",
+			privs = tp_priv,
+			func = function(name,param)
+				local player = minetest.get_player_by_name(name)
+				if not(player) then
+					return false, "Not a player!"
+				end
+				local mpos = pos_marker.get("\\SERVER\\",param)
+				if mpos then
+					player:set_pos(mpos)
+					return true, "The marker "..param.." is at "..minetest.pos_to_string(mpos)
+				end
+				return false, "No this marker!"
+			end
+		},
+	},
 })
 
 minetest.register_chatcommand("gmarks",minetest.registered_chatcommands.gmarker)
-
-
+minetest.register_chatcommand("marks",minetest.registered_chatcommands.marker)
